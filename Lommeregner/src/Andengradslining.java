a187ed3342116d9e0b2c7cac26a595ad90d45425
Emil Thostrup Pedersen <steamman0103@gmail.com>
2023-01-11 13:34:00 +0100

    Andengrandslingning færdi hvor der brugs contruter

--- Lommeregner/src/Andengradslining.java
+++ Lommeregner/src/Andengradslining.java
@@ -1,9 +1,21 @@
 public class Andengradslining {
-    
+    int a;
+    int b;
+    int c; 
+    static double x1,x2;
+  
+    public Andengradslining() {
+        a = 5;
+        b = 6;
+        c = -5;
+        x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+        x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    }
+  
     public static void main(String[] args) {
         new Andengradslining();
+
+        System.out.println("x1 = " + x1);
+        System.out.println("x2 = " + x2);
     }
-
-
-}
-
+  }
